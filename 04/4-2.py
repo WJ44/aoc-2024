7ca7316651aca7a359b32b6788c7c9e9c5549357
Wesley Joosten <github@wjoosten.nl>
2024-12-04 11:41:42 +0100

    Cleanup

--- 04/4-2.py
+++ 04/4-2.py
@@ -9,7 +9,7 @@ for y in range(len(rows) - 2):
         diag1 = "" + rows[y][x] + rows[y + 1][x + 1] + rows[y + 2][x + 2]
         diag2 = "" + rows[y + 2][x] + rows[y + 1][x + 1] + rows[y][x + 2]
 
-        if (diag1 == "MAS" or diag1 == "SAM") and (diag2 == "MAS" or diag2 == "SAM"):
+        if diag1 in ["MAS", "SAM"] and diag2 in ["MAS", "SAM"]:
             count += 1
 
 print(count)

c2caf44b72a337cd27b5bf85e3dc68256f561281
Wesley Joosten <github@wjoosten.nl>
2024-12-08 13:10:49 +0100

    Cleanup

--- 08/8-1.py
+++ 08/8-1.py
@@ -15,37 +15,11 @@ with open("./08/input.txt", "r", encoding="utf-8") as file:
 antinodes = set()
 for frequency, locations in antennas.items():
     for loc1, loc2 in combinations(locations, 2):
-        antinode = (loc1[0] + 2 / 3 * (loc2[0] - loc1[0]), loc1[1] + 2 / 3 * (loc2[1] - loc1[1]))
-        if (
-            int(antinode[0]) == antinode[0]
-            and int(antinode[1]) == antinode[1]
-            and 0 <= antinode[0] <= bounds[0]
-            and 0 <= antinode[1] <= bounds[1]
-        ):
-            antinodes.add(antinode)
-        antinode = (loc2[0] - 2 / 3 * (loc2[0] - loc1[0]), loc2[1] - 2 / 3 * (loc2[1] - loc1[1]))
-        if (
-            int(antinode[0]) == antinode[0]
-            and int(antinode[1]) == antinode[1]
-            and 0 <= antinode[0] <= bounds[0]
-            and 0 <= antinode[1] <= bounds[1]
-        ):
-            antinodes.add(antinode)
         antinode = (loc1[0] + 2 * (loc2[0] - loc1[0]), loc1[1] + 2 * (loc2[1] - loc1[1]))
-        if (
-            int(antinode[0]) == antinode[0]
-            and int(antinode[1]) == antinode[1]
-            and 0 <= antinode[0] <= bounds[0]
-            and 0 <= antinode[1] <= bounds[1]
-        ):
+        if 0 <= antinode[0] <= bounds[0] and 0 <= antinode[1] <= bounds[1]:
             antinodes.add(antinode)
         antinode = (loc2[0] - 2 * (loc2[0] - loc1[0]), loc2[1] - 2 * (loc2[1] - loc1[1]))
-        if (
-            int(antinode[0]) == antinode[0]
-            and int(antinode[1]) == antinode[1]
-            and 0 <= antinode[0] <= bounds[0]
-            and 0 <= antinode[1] <= bounds[1]
-        ):
+        if 0 <= antinode[0] <= bounds[0] and 0 <= antinode[1] <= bounds[1]:
             antinodes.add(antinode)
 
 print(len(antinodes))
